30e278945cd3763001cfbeba9e64ba5740a4613d
apiwonska <annapiwonska1@gmail.com>
2021-10-05 12:14:28 +0200

    Add smooth scrolling and spying active section

--- src/shared/Navigation/index.tsx
+++ src/shared/Navigation/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-scroll';
 import Icon, { iconEnum } from 'assets/Icon';
+
 import styles from './Navigation.module.css';
 
 const Navigation: React.FC = () => (
@@ -13,29 +15,69 @@ const Navigation: React.FC = () => (
     <nav className={styles.nav_main}>
       <ul className={styles.nav_list}>
         <li className={styles.nav_item}>
-          <a href="#home" className={styles.nav_link}>
+          <Link
+            to="home"
+            className={styles.nav_link}
+            activeClass={styles.nav_link__active}
+            spy
+            smooth
+            offset={0}
+            duration={500}
+          >
             Home
-          </a>
+          </Link>
         </li>
         <li className={styles.nav_item}>
-          <a href="#about" className={styles.nav_link}>
+          <Link
+            to="about"
+            className={styles.nav_link}
+            activeClass={styles.nav_link__active}
+            spy
+            smooth
+            offset={0}
+            duration={500}
+          >
             About
-          </a>
+          </Link>
         </li>
         <li className={styles.nav_item}>
-          <a href="#skills" className={styles.nav_link}>
+          <Link
+            to="skills"
+            className={styles.nav_link}
+            activeClass={styles.nav_link__active}
+            spy
+            smooth
+            offset={0}
+            duration={500}
+          >
             Skills
-          </a>
+          </Link>
         </li>
         <li className={styles.nav_item}>
-          <a href="#projects" className={styles.nav_link}>
+          <Link
+            to="projects"
+            className={styles.nav_link}
+            activeClass={styles.nav_link__active}
+            spy
+            smooth
+            offset={0}
+            duration={500}
+          >
             Projects
-          </a>
+          </Link>
         </li>
         <li className={styles.nav_item}>
-          <a href="#contact" className={styles.nav_link}>
+          <Link
+            to="contact"
+            className={styles.nav_link}
+            activeClass={styles.nav_link__active}
+            spy
+            smooth
+            offset={0}
+            duration={500}
+          >
             Contact
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
