b5897a3b8afe44edd65137921c24afa57139fe6f
apiwonska <annapiwonska1@gmail.com>
2021-10-12 20:14:30 +0200

    Move Cursor and Navbar components inside PageLayout

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Skills from 'sections/Skills';
 import Projects from 'sections/Projects';
 import Contact from 'sections/Contact';
 import PageLayout from 'shared/PageLayout';
-import Navigation from 'shared/Navigation';
-import Cursor from 'shared/Cursor';
 import 'App.css';
 
 const App: React.FC = () => {
@@ -17,14 +15,12 @@ const App: React.FC = () => {
   return (
     <>
       <PageLayout>
-        <Navigation />
         <Home />
         <About />
         <Skills />
         <Projects />
         <Contact />
       </PageLayout>
-      <Cursor />
     </>
   );
 };
--- src/shared/PageLayout/index.tsx
+++ src/shared/PageLayout/index.tsx
@@ -1,12 +1,18 @@
+import Navigation from 'shared/Navigation';
+import Cursor from 'shared/Cursor';
 import styles from './PageLayout.module.css';
 
 const PageLayout: React.FC = ({ children }) => (
-  <div className={styles.wrapper}>
-    <main>{children}</main>
-    <footer className={styles.footer}>
-      Designed and executed by Anna Piwonska
-    </footer>
-  </div>
+  <>
+    <div className={styles.wrapper}>
+      <Navigation />
+      <main>{children}</main>
+      <footer className={styles.footer}>
+        Designed and executed by Anna Piwonska
+      </footer>
+    </div>
+    <Cursor />
+  </>
 );
 
 export default PageLayout;
