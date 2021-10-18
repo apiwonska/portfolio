1ee998f4cee21500bc594f24e5c0fdbd6b8d4535
apiwonska <annapiwonska1@gmail.com>
2021-10-18 17:28:59 +0200

    Add new design of about section

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -1,25 +1,37 @@
 import SectionHeader from 'shared/SectionHeader';
-import photo from 'assets/my-photo.png';
+import photo from 'assets/my-photo.svg';
 import styles from './About.module.css';
 
 const About: React.FC = () => (
-  <section id="about">
-    <SectionHeader>About</SectionHeader>
-
-    <div className={styles.description}>
-      <p>
-        I’m a self-taught developer based in Warsaw, Poland. I love to create
-        beautiful, functional and accessible applications. I’m interested mostly
-        in front-end part. Former structural enginer.
-      </p>
-      <p>
-        Except passion for coding I enjoy afro-cuban dance, spanish language,
-        travelling and painting.
-      </p>
+  <section id="about" className="section">
+    <div className="container">
+      <SectionHeader>About</SectionHeader>
     </div>
-    <div className={styles.photo}>
-      <div className={styles.img}>
-        <img src={photo} alt="" />
+
+    <div className={styles.content_bg}>
+      <div className="container">
+        <div className={styles.content_wrapper}>
+          <div className={styles.photo_wrapper}>
+            <img src={photo} alt="" />
+          </div>
+          <div className={styles.description_wrapper}>
+            <div className={styles.description}>
+              <p>
+                I’m a self-taught developer based in Warsaw, Poland. I love to
+                create beautiful, functional and accessible applications. I’m
+                interested mostly in front-end part. Former structural enginer.
+              </p>
+              <p>
+                Except passion for coding I enjoy afro-cuban dance, spanish
+                language, travelling and painting.
+              </p>
+            </div>
+            <div className={styles.additionalInfo}>
+              <p>Location: Warsaw, Poland</p>
+              <p>Languages: Polish (native), English</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
