0c37458cc5dab512ff7a73d919cd1d65a719b0bc
apiwonska <annapiwonska1@gmail.com>
2021-10-19 13:33:35 +0200

    Add new design of skills section

--- src/sections/Skills/index.tsx
+++ src/sections/Skills/index.tsx
@@ -1,25 +1,29 @@
 import SectionHeader from 'shared/SectionHeader';
 import { nanoid } from 'nanoid';
 import styles from './Skills.module.css';
-import skills, { ISkills } from './skills';
+import skills from './skills';
 
 const Skills: React.FC = () => (
-  <section id="skills">
-    <SectionHeader>Skills</SectionHeader>
+  <section id="skills" className="section">
+    <div className="container">
+      <SectionHeader>Skills</SectionHeader>
+    </div>
 
-    <div className="">
-      {Object.keys(skills).map((cat) => (
-        <div key={nanoid()} className="">
-          <h3 className={styles.skills_title}>{cat}</h3>
-          <ul className={styles.skills_list}>
-            {skills[cat].map((skill) => (
-              <li className={styles.skills_item} key={nanoid()}>
-                {skill}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+    <div className={styles.content_bg}>
+      <div className={styles.content_wrapper}>
+        {Object.keys(skills).map((cat) => (
+          <div key={nanoid()} className={styles.skills_group}>
+            <h3 className={styles.skills_title}>{cat}</h3>
+            <ul className={styles.skills_list}>
+              {skills[cat].map((skill) => (
+                <li className={styles.skills_item} key={nanoid()}>
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   </section>
 );
