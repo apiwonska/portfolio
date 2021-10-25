7be0647fd75ae6b98e9cfee2caa148deb489cb1c
apiwonska <annapiwonska1@gmail.com>
2021-10-25 23:01:49 +0200

    Add section header animation and change triangle decorative element to braces

--- src/shared/SectionHeader/index.tsx
+++ src/shared/SectionHeader/index.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
 import styles from './SectionHeader.module.scss';
 
 type Props = {
   children: string;
 };
 
-const SectionHeader: React.FC<Props> = ({ children }) => (
-  <h2 className={styles.sectionHeader}>{children}</h2>
-);
+const SectionHeader: React.FC<Props> = ({ children }) => {
+  const headerRef = useRef<null | HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const intersection = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          headerRef.current?.classList.add(styles.sectionHeader_show);
+        } else {
+          headerRef.current?.classList.remove(styles.sectionHeader_show);
+        }
+      });
+    });
+    if (headerRef.current) {
+      intersection.observe(headerRef.current);
+    }
+  }, []);
+
+  const textToLetters = children.split('').map((letter) => {
+    const style = !letter ? styles.space : styles.letter;
+    return (
+      <span className={style} key={nanoid()}>
+        {letter}
+      </span>
+    );
+  });
+
+  return (
+    <h2 className={styles.sectionHeader} ref={headerRef}>
+      {textToLetters}
+    </h2>
+  );
+};
 
 export default SectionHeader;
